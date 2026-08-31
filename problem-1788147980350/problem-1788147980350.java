133641a485294d04b0cf5fdfad54640537e726b8
24104162-cpu <24104162@drngpit.ac.in>
2026-08-31 09:16:21 +0530

    Create

---
+++ problem-1788147980350/problem-1788147980350.java
@@ -0,0 +1,10 @@
+// Last updated: 8/31/2026, 9:16:20 AM
+1class Solution {
+2    public int minBishopMoves(int[] source, int[] target) {
+3        int sr=source[0],sc=source[1];
+4        int tr=target[0],tc=target[1];
+5        if((sr+sc)%2!=(tr+tc)%2) return -1;
+6        if(Math.abs(sr-tr)==Math.abs(sc-tc)) return 1;
+7        return 2;
+8    }
+9}
